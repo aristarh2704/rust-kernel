2fe246cd8d4aa1cbd8aba1b9700cfe19052a47aa
Feshchukov Aristarh <fe.arik@yandex.ru>
2018-02-08 16:58:49 +0300

    Send multiboot info pointer to kernel

--- arch/x86/entry/entry.asm
+++ arch/x86/entry/entry.asm
@@ -3,25 +3,25 @@
 ;nasm directive - 32 bit
 bits 32
 section .text
+global start
+start:
         ;multiboot spec
         align 4
         dd 0x1BADB002            ;магические числа
         dd 64                  ;флаги
         dd - (0x1BADB002 + 64) ;контрольная сумма. мч+ф+кс должно равняться нулю
  
-global start
+
 extern _kmain         ;kmain определена во внешнем файле
  
-start:
+
   cli ;блокировка прерываний
-  mov esp, stack_space ;указатель стека
-  mov eax,[ebx+44]
-  push eax
-  mov eax, [ebx+48]
+  mov esp, end ;указатель стека
+  mov eax, ebx
   push eax
   call _kmain
   hlt ;остановка процессора
 
 section .bss
 resb 8192 ;8KB на стек
-stack_space:
+end:
