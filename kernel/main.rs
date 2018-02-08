d971b68a78b22da34973d3ff1fd42aa22a1e951e
Feshchukov Aristarh <fe.arik@yandex.ru>
2018-02-08 17:10:18 +0300

    Kernel output boot loader name now

--- kernel/main.rs
+++ kernel/main.rs
@@ -1,42 +1,51 @@
 #![no_std]
 #![feature(lang_items)]
+
 extern crate console;
 extern crate rlibc;
-//extern crate mem;
-use console::Console;
-use core::slice;
+extern crate multiboot;
+
 use core::fmt::Write;
+use multiboot::*;
+use console::writer;
 
 #[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personality() {}
 #[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {
-    let mut buffer=Console::new(unsafe{
-        slice::from_raw_parts_mut(0x0b8000 as *mut u16,2000)
-    });
-    buffer.clean();
-    let _result=write!(buffer,"PANIC!!!!");
+    console::WRITER.lock().write_str("PANIC!!!");
     loop{}
 }
 
-#[no_mangle]
-pub extern "C" fn kmain(x:u32,y:u32) {
-    let mut buffer=Console::new(unsafe{
-        slice::from_raw_parts_mut(0x0b8000 as *mut u16,2000)
-    });
-    buffer.clean();
-    let mut _result=write!(buffer,"{} {}\n",x,y);
-    let mmap:&mut[u32];
-    let words=y/4;
-    unsafe{
-        mmap=slice::from_raw_parts_mut(x as *mut u32,words as usize);
-    };
-    let structs=words/6;
-    _result=write!(buffer,"Structs: {}\n",structs);
-    for i in 0..structs{
-        _result=write!(buffer,"Number: {}\n  Base addr: 0x{:X}\n  Length: 0x{:X}\n  Type: {}\n",
-        i,
-        mmap[(i*6+1) as usize],
-        mmap[(i*6+3) as usize],
-        mmap[(i*6+5) as usize]);
-    };
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => (WRITER.lock().write_fmt(format_args!($($arg)*)))
+}
+#[macro_export]
+macro_rules! println {
+    () => (print!("\n"));
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
 }
 
+#[no_mangle]
+pub extern "C" fn kmain(multiboot_addr: *const MultiBoot) {
+    writer().clear();
+    println!("Hello world");
+    let mb_info=unsafe{
+        &*multiboot_addr
+    };
+    print!("My loader is: ");
+    println!("{}",unsafe{to_str(mb_info.loader)});
+}
+
+unsafe fn to_str(addr:*const u8)->&'static str{
+    let mut index=0isize;
+    loop{
+        let byte=*addr.offset(index);
+        if byte==0{
+            break;
+        }
+        index+=1;
+    }
+    let slice=core::slice::from_raw_parts(addr,index as usize);
+    core::str::from_utf8(slice).unwrap() // TODO
+}
