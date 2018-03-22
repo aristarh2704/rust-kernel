7ea689d67d7e63a0a7f0b28c73fb0ea16352b324
Feshchukov Aristarh <fe.arik@yandex.ru>
2018-03-22 11:19:17 +0300

    Refactoring code

--- multiboot/lib.rs
+++ multiboot/lib.rs
@@ -2,30 +2,27 @@
 extern crate spin;
 // Description of Multiboot information structure is in 
 // https://www.gnu.org/software/grub/manual/multiboot/multiboot.html#Boot-information-format
-
 #[repr(packed)]
-pub struct MultiBoot<'a>{
-    flags:  u32,
+pub struct MultiBoot{
+    pub flags:  u32,
     mem:    LUMem,
-    boot:   &'a BootDevice,
-    cmdline: *const u8,
-    mods:   Modules,
+    boot:   &'static BootDevice,
+    pub cmdline: *const u8,
+    pub mods:   Modules,
     syms:   SymbolTable,
     pub mmap:   Mmap,
     drives: Drives,
-    config: &'a BIOSConfigTable,
+    config: &'static BIOSConfigTable,
     pub loader: *const u8,
-    apm:    &'a APMTable,
+    apm:    &'static APMTable,
     vbe:    VBE,
     //fb:     Option<FrameBuffer>,
 }
-
 #[repr(packed)]
 pub struct LUMem{
     pub lower: u32,
     pub upper: u32,
 }
-
 #[repr(packed)]
 pub struct BootDevice{
     pub part3: u8,
@@ -33,39 +30,36 @@ pub struct BootDevice{
     pub part1: u8,
     pub drive: u8,
 }
-
 #[repr(packed)]
 pub struct Modules{
-    pub mods_count: u32,
-    pub mods_addr:  u32,
+    pub count: u32,
+    pub addr:  u32,
     // TODO: заглушка
 }
-
 #[repr(packed)]
 pub struct SymbolTable{
     pub num: [u32;4]
 }
-
 #[repr(packed)]
 pub struct Mmap{
     pub length: u32,
     pub addr:   u32, //Указатель на массив Frame
 }
-
+#[repr(packed)]
 pub struct Frame{
     pub size:   u32,
     pub addr:   u32,
+    _x:          u32,
     pub length: u32,
-    pub flag:   u8,
+    _y:          u32,
+    pub flag:   u32,
 }
-
 #[repr(packed)]
 pub struct Drives{
     pub length: u32,
     pub addr:   u32, //Указатель на массив Drive
 }
-
-pub struct Drive<T: [u16]{
+pub struct Drive<T>{
     pub number:    u8,
     pub mode:      u8,
     pub cylinders: u16,
@@ -73,12 +67,10 @@ pub struct Drive<T: [u16]{
     pub sectors:   u8,
     pub ports:     T,
 }
-
 #[repr(packed)]
 pub struct BIOSConfigTable{
 //TODO
 }
-
 #[repr(packed)]
 pub struct APMTable{
     pub version:         u16,
@@ -91,7 +83,6 @@ pub struct APMTable{
     pub code_seg_16_len: u16,
     pub data_seg_len:    u16,
 }
-
 #[repr(packed)]
 pub struct VBE{ 
     pub ctrl_info:     u32, // Формально, это 2 указателя на какие-то структуры. Пока не будем трогать
@@ -104,5 +95,13 @@ pub struct VBE{
 pub struct FrameBuffer{
 //TODO
 }
-
+impl MultiBoot{
+    pub fn mmap(&self)->Option<&'static [Frame]>{
+        unsafe{
+            Some(core::slice::from_raw_parts(
+                self.mmap.addr as *const Frame,
+                (self.mmap.length as usize)/core::mem::size_of::<Frame>()
+        ))}
+    }
+}
 pub static mut MULTIBOOT:*const MultiBoot=0 as *const MultiBoot;
